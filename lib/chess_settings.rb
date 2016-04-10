0f304b19a1306b993a6ed0e566a5acafafbb06e6
Cillas <cillassubira@gmail.com>
2016-04-10 19:43:15 +0200

    Add modifications to include complex board moves and corresponding txt files

--- lib/chess_settings.rb
+++ lib/chess_settings.rb
@@ -3,113 +3,125 @@
 #Rook, Bishop, Queen, King, Knight and Pawn Classes < ChessPiece
 
 class ChessPiece
-	def initialize(present_position)
-		@present_position = present_position
-	end
-	def difference_axis(present_position, expected_position)
- 		diff_x = expected_position[0] - @present_position[0]
-		diff_y = expected_position[1] - @present_position[1]
-		[diff_x.abs, diff_y.abs]
- 	end
-	def check_diagonally (present_position, expected_position)
-		diff_x, diff_y = difference_axis(present_position, expected_position)
-		diff_x == diff_y
- 	end
- 	def check_straight(present_position, expected_position)	
-		present_position[0] == expected_position[0] || present_position[1] == expected_position[1]
- 	end
+  def initialize(present_position)
+    @present_position = present_position
+  end
+  def difference_axis(present_position, expected_position)
+    diff_x = expected_position[0] - @present_position[0]
+    diff_y = expected_position[1] - @present_position[1]
+    [diff_x.abs, diff_y.abs]
+  end
+  def check_diagonally (present_position, expected_position)
+    diff_x, diff_y = difference_axis(present_position, expected_position)
+    diff_x == diff_y
+  end
+  def check_straight(present_position, expected_position) 
+    present_position[0] == expected_position[0] || present_position[1] == expected_position[1]
+  end
 end
 
 class Rook < ChessPiece
-	def check_move(expected_position)
-		@expected_position = expected_position
-		if check_straight(@present_position, @expected_position)
-			true
-		else
-			false
-		end
-	end
+  def check_move(expected_position)
+    @expected_position = expected_position
+    if check_straight(@present_position, @expected_position)
+      true
+    else
+      false
+    end
+  end
 end
 
 class Bishop < ChessPiece
-	def check_move(expected_position)
-		@expected_position = expected_position
-		if check_diagonally(@present_position, @expected_position)
-			true
-		else
-			false
-		end
-	end
+  def check_move(expected_position)
+    @expected_position = expected_position
+    if check_diagonally(@present_position, @expected_position)
+      true
+    else
+      false
+    end
+  end
 end
 
 class Queen < ChessPiece
-	def check_move(expected_position)
-		@expected_position = expected_position
-		if check_diagonally(@present_position, @expected_position) != check_straight(@present_position, @expected_position)
-			true
-		else
-			false
-		end
-	end
+  def check_move(expected_position)
+    @expected_position = expected_position
+    if check_diagonally(@present_position, @expected_position) != check_straight(@present_position, @expected_position)
+      true
+    else
+      false
+    end
+  end
 end
 
 class King < ChessPiece
-	def check_move(expected_position)
-		@expected_position = expected_position
-		diff_x, diff_y = difference_axis(@present_position, @expected_position)
-		if diff_x <= 1 && diff_y <= 1
-			true
-		else
-			false
-		end
-	end
+  def check_move(expected_position)
+    @expected_position = expected_position
+    diff_x, diff_y = difference_axis(@present_position, @expected_position)
+    if diff_x <= 1 && diff_y <= 1
+      true
+    else
+      false
+    end
+  end
 end
 
 class Knight < ChessPiece
-	def check_move(expected_position)
-		@expected_position = expected_position
-		diff_x, diff_y = difference_axis(@present_position, @expected_position)
-		if diff_x == 1 && diff_y == 2
-			veredict = true
-		elsif diff_x == 2 && diff_y == 1
-			true
-		else
-			false
-		end
-	end
+  def check_move(expected_position)
+    @expected_position = expected_position
+    diff_x, diff_y = difference_axis(@present_position, @expected_position)
+    if diff_x == 1 && diff_y == 2
+      veredict = true
+    elsif diff_x == 2 && diff_y == 1
+      true
+    else
+      false
+    end
+  end
 end
 
 class Pawn < ChessPiece
-	def check_move(expected_position, colour)
-		@expected_position = expected_position
-		@colour = colour
-		diff_x = @expected_position[0] - @present_position[0]
-		if @colour == "b" && diff_x.between?(1,2)
-			check_straight(@present_position, @expected_position)
-		elsif @colour == "w" && diff_x.between?(-2,-1)
-			check_straight(@present_position, @expected_position)
-		else
-			false
-		end
-	end
+  def check_move(expected_position, colour)
+    @expected_position = expected_position
+    @colour = colour
+    diff_x = @expected_position[0] - @present_position[0]
+    if @colour == "b" && diff_x.between?(1,2)
+      check_straight(@present_position, @expected_position)
+    elsif @colour == "w" && diff_x.between?(-2,-1)
+      check_straight(@present_position, @expected_position)
+    else
+      false
+    end
+  end
 end
 
 class Board
-	attr_accessor :board
-	def initialize
-		@board = []
-	end
-	def simple
-		@board = [
-			[:bR, :bN, :bB, :bQ, :bK, :bB, :bN, :bR],
-			[:bP, :bP, :bP, :bP, :bP, :bP, :bP, :bP],
-			[nil, nil, nil, nil, nil, nil, nil, nil],
-			[nil, nil, nil, nil, nil, nil, nil, nil],
-			[nil, nil, nil, nil, nil, nil, nil, :wP],
-			[nil, nil, nil, nil, nil, nil, nil, nil],
-			[:wP, :wP, :wP, :wP, :wP, :wP, :wP, :wP],
-			[:wR, :wN, :wB, :wQ, :wK, :wB, :wN, :wR]
-		]
-	end
+  attr_accessor :board
+  def initialize
+    @board = []
+  end
+  def simple
+    @board = [
+      [:bR, :bN, :bB, :bQ, :bK, :bB, :bN, :bR],
+      [:bP, :bP, :bP, :bP, :bP, :bP, :bP, :bP],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [nil, nil, nil, nil, nil, nil, nil, :wP],
+      [nil, nil, nil, nil, nil, nil, nil, nil],
+      [:wP, :wP, :wP, :wP, :wP, :wP, :wP, :wP],
+      [:wR, :wN, :wB, :wQ, :wK, :wB, :wN, :wR]
+    ]
+  end
+  def complex
+    @board = [
+      [:bK, nil, nil, nil, nil, :bB, nil, nil],
+      [nil, nil, nil, nil, nil, :bP, nil, nil],
+      [nil, :bP, :wR, nil, :wB, nil, :bN, nil],
+      [:wN, nil, :bP, :bR, nil, nil, nil, :wP],
+      [nil, nil, nil, nil, :wK, :wQ, nil, :wP],
+      [:wP, nil, :bB, :wN, :wP, nil, nil, nil],
+      [nil, :wP, :bQ, nil, nil, :wP, nil, nil],
+      [nil, nil, nil, nil, nil, :wB, nil, nil]
+    ]
+  end
 end
 
--- lib/chess_validator.rb
+++ lib/chess_validator.rb
@@ -6,83 +6,86 @@ require './chess_settings.rb'
 
 class Game
 
-	def initialize(board, plays_file, veredicts_file)
-		@board = board
-		@veredicts = []
-		@plays = download_plays_txt(plays_file)	
-		@veredicts_file = veredicts_file
-	end
+  def initialize(board, plays_file, veredicts_file)
+    @board = board
+    @veredicts = []
+    @plays = download_plays_txt(plays_file) 
+    @veredicts_file = veredicts_file
+  end
 
-	def start
-		@plays.each_with_index do |move, index|
-			if index%2 == 0
-				@ending_position_x, @ending_position_y = conv_chess_to_cartesian(@plays[index+1])
-				ending_position_check = check_piece_board(@ending_position_x, @ending_position_y)
-				
-				if ending_position_check == nil
-					@present_position_x, @present_position_y = conv_chess_to_cartesian(move)
-					piece = check_piece_board(@present_position_x, @present_position_y)
-					veredict = guess_piece(piece)
-				else
-					veredict = false
-				end
-				conversor_veredict(veredict)
-			end
-			save_veredict_txt(@veredicts, @veredicts_file)
-		end
-	end
+  def start
+    @plays.each_with_index do |move, index|
+      if index%2 == 0
+        @ending_position_x, @ending_position_y = conv_chess_to_cartesian(@plays[index+1])
+        ending_position_check = check_piece_board(@ending_position_x, @ending_position_y)
+        
+        if ending_position_check == nil
+          @present_position_x, @present_position_y = conv_chess_to_cartesian(move)
+          piece = check_piece_board(@present_position_x, @present_position_y)
+          veredict = guess_piece(piece)
+        else
+          veredict = false
+        end
+        conversor_veredict(veredict)
+      end
+      save_veredict_txt(@veredicts, @veredicts_file)
+    end
+  end
 
-	def download_plays_txt(plays_file)
-		plays = IO.read(plays_file).tr(" ", "\n")
-		plays = plays.split("\n")
-	end
+  def download_plays_txt(plays_file)
+    plays = IO.read(plays_file).tr(" ", "\n")
+    plays = plays.split("\n")
+  end
 
-	def conversor_veredict(veredict)
-		veredict ? (@veredicts << "LEGAL") : (@veredicts << "ILLEGAL")
-	end
+  def conversor_veredict(veredict)
+    veredict ? (@veredicts << "LEGAL") : (@veredicts << "ILLEGAL")
+  end
 
-	def save_veredict_txt(veredicts, veredicts_file)
-		IO.write(@veredicts_file,veredicts.join("\n"))
-	end
+  def save_veredict_txt(veredicts, veredicts_file)
+    IO.write(@veredicts_file,veredicts.join("\n"))
+  end
 
-	def conv_chess_to_cartesian(position)
-		cartesian_x = [8, 7, 6, 5, 4, 3, 2, 1].index(position[1].to_i)
-		cartesian_y = ["a","b","c","d","e","f","g","h"].index(position[0])
-		[cartesian_x, cartesian_y]
-	end
+  def conv_chess_to_cartesian(position)
+    cartesian_x = [8, 7, 6, 5, 4, 3, 2, 1].index(position[1].to_i)
+    cartesian_y = ["a","b","c","d","e","f","g","h"].index(position[0])
+    [cartesian_x, cartesian_y]
+  end
 
-	def check_piece_board(position_x, position_y)
-		piece = @board[position_x][position_y]
-	end
+  def check_piece_board(position_x, position_y)
+    piece = @board[position_x][position_y]
+  end
 
-	def guess_piece(piece)
-		case piece
-		when :bR, :wR
-			rook = Rook.new([@present_position_x, @present_position_y])
-			rook.check_move([@ending_position_x, @ending_position_y])
-		when :bN, :wN
-			knight = Knight.new([@present_position_x, @present_position_y])
-			knight.check_move([@ending_position_x, @ending_position_y])
-		when :bB, :wB
-			bishop = Bishop.new([@present_position_x, @present_position_y])
-			bishop.check_move([@ending_position_x, @ending_position_y])
-		when :bQ, :wQ
-			queen = Queen.new([@present_position_x, @present_position_y])
-			queen.check_move([@ending_position_x, @ending_position_y])
-		when :bK, :WK
-			king = King.new([@present_position_x, @present_position_y])
-			king.check_move([@ending_position_x, @ending_position_y])
-		when :bP
-			pawn = Pawn.new([@present_position_x, @present_position_y])
-			pawn.check_move([@ending_position_x, @ending_position_y], "b")
-		when :wP
-			pawn = Pawn.new([@present_position_x, @present_position_y])
-			pawn.check_move([@ending_position_x, @ending_position_y], "w")
-		else
-			false
-		end
-	end
+  def guess_piece(piece)
+    case piece
+    when :bR, :wR
+      rook = Rook.new([@present_position_x, @present_position_y])
+      rook.check_move([@ending_position_x, @ending_position_y])
+    when :bN, :wN
+      knight = Knight.new([@present_position_x, @present_position_y])
+      knight.check_move([@ending_position_x, @ending_position_y])
+    when :bB, :wB
+      bishop = Bishop.new([@present_position_x, @present_position_y])
+      bishop.check_move([@ending_position_x, @ending_position_y])
+    when :bQ, :wQ
+      queen = Queen.new([@present_position_x, @present_position_y])
+      queen.check_move([@ending_position_x, @ending_position_y])
+    when :bK, :WK
+      king = King.new([@present_position_x, @present_position_y])
+      king.check_move([@ending_position_x, @ending_position_y])
+    when :bP
+      pawn = Pawn.new([@present_position_x, @present_position_y])
+      pawn.check_move([@ending_position_x, @ending_position_y], "b")
+    when :wP
+      pawn = Pawn.new([@present_position_x, @present_position_y])
+      pawn.check_move([@ending_position_x, @ending_position_y], "w")
+    else
+      false
+    end
+  end
 end
 
 board_simple = Board.new.simple
 game_simple = Game.new(board_simple, 'simple_moves.txt', 'simple_results.txt').start
+
+board_complex = Board.new.complex
+game_complex = Game.new(board_complex, 'complex_moves.txt', 'complex_results.txt').start
